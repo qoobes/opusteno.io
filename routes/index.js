f0b07a6fa1f1ac8bf3f087cf7d84f56e9616af97
theqoobee <qoobeethegreat@gmail.com>
2020-04-27 23:28:43 +0200

    Added the actual route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const temps = require('../templ')
 
 // The Simple routes
 
+function isConfirmed (req, res, next) {
+    const confirmed = req.session.verified === undefined ? false : req.session.verified.state
+    if (!confirmed) next({ message: 'Vas token nije validan', status: '403'  })
+    else next()
+}
+
 router.get('/', (req, res, next) => {
   // I will override the inputValue part of temps if the user already has a session
   res.render('index', { temps })
@@ -11,6 +17,10 @@ router.get('/', (req, res, next) => {
 
 router.get('/about', (req, res, next) => {
   res.render('about', { temps })
+})
+
+router.get('/form', isConfirmed, (req, res, next) => {
+  res.render('form', { temps })
 })
 
 module.exports = router;
