cac32862278785e12f82de110c3f72a92fb5478c
theqoobee <qoobeethegreat@gmail.com>
2020-04-28 18:19:51 +0200

    Completed mail clinet

--- mailClient.js
+++ mailClient.js
@@ -1,10 +1,31 @@
-const mailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 require('dotenv').config()
 
+let from = 'qoobestestmail@gmail.com'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_ADDRESS,
+    pass: process.env.MAIL_PASS
+  }
+})
+
 const secret = process.env.CONFIRM_SECRET
+const sendConfirmation = email => {
+  let mailOptions = {
+    from,
+    to: email,
+    subject: 'Please confirm your email for opusteno.io',
+    text: '<a href="https://link.com">This is a lilnk to the confirmation thing</a>'
+  }
 
-function sendConfirmation(email) {
-  
-
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log(`An error has occured: \n ${err}`)
+      return false
+    } else return true
+  })
 }
 
+module.exports = sendConfirmation
