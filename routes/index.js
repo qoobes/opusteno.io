d4bb81a73bd4b56e9c9bbb9db10b0b620615becb
theqoobee <qoobeethegreat@gmail.com>
2020-04-29 00:48:16 +0200

    More or less the same thing, i had to fiddle aorund tho

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const temps = require('../templ')
 const mailClient = require('../mailClient')
+var nigga = 0
+
+// Setting up the list of mails 
+// This is not supposed to be persistent for longer than 5 minutes, so keeping it ram is okay
+// It's a hacky solution to a rpbolem lolo lol ol ol o lo
+var sentMails = new Array()
+var sentMailTimestamps = new Array()
 
 // The Simple routes
 
@@ -14,6 +21,8 @@ function isConfirmed (req, res, next) {
 router.get('/', (req, res, next) => {
   // I will override the inputValue part of temps if the user already has a session
   res.render('index', { temps })
+  nigga++
+  console.log(nigga)
 })
 
 router.get('/about', (req, res, next) => {
@@ -34,12 +43,18 @@ router.post('/auth', (req, res, next) => {
   console.log(email)
   
   // Send the confirmation mail
-  let error = mailClient(email)
+  let error = mailClient(email, sentMails, sentMailTimestamps)
+  console.log(`The one from the router: \n ${sentMails}`)
   if (error) {
     if (error.message === 1005) res.send(error.message)
     console.log(error.message)
     if (process.env.MODE === 'dev') res.send(error.message)
-  } else res.send('success')
+  } else {
+    let num1 = sentMails.push(email)
+    let num2 = sentMailTimestamps.push(Date.now())
+    if (num1 !== num2) console.log('BIG ASS FUCKING ERROR WITH THE MAIL LIST')
+    res.send('success')
+  }
 
 })
 
