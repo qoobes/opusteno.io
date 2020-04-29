cd2f0b6ac9892fc66bba0c64529ce100ba8ea5a2
theqoobee <qoobeethegreat@gmail.com>
2020-04-29 20:06:37 +0200

    Mongodb UNGLITCH

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
+const ObjectId = require('mongoose').ObjectId
 const Schema = mongoose.Schema
+
 
 const UserSchema = new Schema({
   email: {
     type: String,
     required: 'Please enter an email'
   },
-  messages: [{ id: ObjectId }]
+  messages: [{ message_id: ObjectId }]
 })
 
 module.exports = mongoose.model('User', UserSchema)
