173c0b3ed7d086c18bd1c6c8e990560700051628
theqoobee <qoobeethegreat@gmail.com>
2020-04-30 19:43:45 +0200

    Creates messages with a PROMISE

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const UserModel = require('../models/user')
+const MessageModel = require('../models/messages')
 const User = mongoose.model('User')
+const Message = mongoose.model('Message')
 
 exports.exists = email => {
   User.exists({ email }, (err, result) => {
@@ -15,13 +17,28 @@ exports.exists = email => {
 
 exports.addUser = email => {
   const new_user = new User({ email })
-  new_user.save((err, event) => { 
+  new_user.save((err, event) => {
     if (err) {
       console.error(err)
-      return false 
+      return false
     } else {
       console.log(`Created a user for ${event.email}`)
       return event
     }
+  })
+}
+
+exports.createMessage = async message => {
+  return new Promise((resolve, reject) => {
+    let new_message = new Message(message)
+    new_message.save((err, event) => {
+      if (err) {
+        console.error(`An error has occured in exports.createMessag: ${err}`)
+        resolve(false)
+      } else {
+        console.log(`A new message has been added: ${event}`)
+        resolve(event)
+      }
+    })
   })
 }
