1eac8c7d2ef3c33b60399540e16a61e95a5a4eeb
theqoobee <qoobeethegreat@gmail.com>
2020-05-03 18:21:49 +0200

    Patched bugs up, better backend, proper adding

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -5,21 +5,20 @@ const User = mongoose.model('User')
 const Message = mongoose.model('Message')
 
 exports.exists = email => {
-  return new Promise((resolve, reject) => {
-  User.exists({ email }, (err, result) => {
-    if (err) {
-      console.error(err)
-      resolve(false)
-    } else {
-      console.log(`THE USER DOES ${result}`)
-      resolve(result)
-    }
-  })
+  return new Promise(resolve => {
+    User.exists({ email }, (err, result) => {
+      if (err) {
+        console.error(err)
+        resolve(false)
+      } else {
+        resolve(result)
+      }
+    })
   })
 }
 
 exports.addUser = async email => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     const new_user = new User({ email })
     new_user.save((err, event) => {
       if (err) {
@@ -34,21 +33,36 @@ exports.addUser = async email => {
 }
 
 exports.createMessage = async (message, email) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     let new_message = new Message(message)
     new_message.save((err, event) => {
       if (err) {
         console.error(`An error has occured in exports.createMessag: ${err}`)
         resolve(false)
       } else {
-        console.log(`A new message has been added: ${event}`)
-        // addMessageToUser(data.id, email)
-        resolve(true)
+        console.log(`A new message has been added`)
+        if (addMessageToUser(event.id, email)) resolve(true)
+        resolve(false)
       }
     })
   })
 }
 
-// function addMessageToUser(id) {
-//   User.findOne()
-// }
+function addMessageToUser(id, email) {
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      console.error(`An error has occured in exports.addMessageToUser: ${err}`)
+      return false
+    } else {
+      user.messages.push(id)
+      console.log(`The id is ${id}`)
+      user.save((err, event) => {
+        if (err) {
+          console.error(`An error has occured in addMessageToUser: ${err}`)
+        } else {
+          return true
+        }
+      })
+    }
+  })
+}
